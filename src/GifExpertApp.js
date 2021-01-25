7e1fac668d41decb32276280d8583bbd5d19a904
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-24 20:15:45 -0500

    comunicacion entre componentes, se pueden mandar funciones :o no sabia eso, por eso lo coloco en el commit

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,20 +1,20 @@
 import { useState } from "react"
+import AddCategory from "./components/AddCategory"
 
 const GifExpertApp = () => {
-    //const categories = ['One Punch', 'Naruto', 'Shingeki No Kyojin'];
-
+    
     const [categories, setcategories] = useState(['One Punch', 'Naruto', 'Shingeki No Kyojin'])
 
-    const handleAdd = (e)=>{
+/*     const handleAdd = (e)=>{
         setcategories([...categories, 'Dr Stone']);
-    }
+    } */
     
     return (
         <>
             <h2>GifExpertApp</h2>  
+            <AddCategory setcategories={setcategories}/>
             <hr/>
 
-            <button onClick={ handleAdd }>Agregar</button>
 
             <ol>   
                 {
